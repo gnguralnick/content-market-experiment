97b71439278ef1d257829cd05f9c974393ac166a
Gabe Guralnick <gnguralnick@gmail.com>
2023-10-08 17:38:10 -0400

    implement combined consumer/producer

---
+++ agents/consumer_producer.py
@@ -0,0 +1,16 @@
+from consumer import Consumer
+from producer import Producer
+import numpy as np
+
+class ConsumerProducer(Consumer, Producer):
+    """
+    A consumer-producer is an agent that can consume content and produce content.
+    """
+    
+    def __init__(self, main_interest: np.ndarray, consumer_topic_interest_function, producer_topic_interest_function, index):
+        Consumer.__init__(self, main_interest, consumer_topic_interest_function, index)
+        Producer.__init__(self, main_interest, producer_topic_interest_function, index)
+    
+    def set_market(self, market):
+        Consumer.set_market(self, market)
+        Producer.set_market(self, market)
