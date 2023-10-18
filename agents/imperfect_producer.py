828d0c9d7c877735628b344fd047858f4583dc10
Gabe Guralnick <gnguralnick@gmail.com>
2023-10-17 22:31:41 -0400

    implement imperfect producer

--- agents/imperfect_producer.py
+++ agents/imperfect_producer.py
@@ -1,24 +1,64 @@
-from typing import cast, TYPE_CHECKING
-if TYPE_CHECKING:
-    from content_market import ContentMarket
+from typing import cast
+
+from agents.influencer import Influencer
 
 import numpy as np
 from agents.producer import Producer
 
+from scipy.optimize import minimize, LinearConstraint
+
+from util import OptimizationTargets
+
 class ImperfectInformationProducer(Producer):
 
-    def __init__(self, index: int, main_interest: np.ndarray, topic_interest_function):
-        super().__init__(index, main_interest, topic_interest_function)
+    def __init__(self, topic_interest_function):
+        super().__init__(topic_interest_function)
+        self.use_imperfect_information = True
+
+    def toggle_imperfect_information(self):
+        self.use_imperfect_information = not self.use_imperfect_information
 
     def utility(self, topic: np.ndarray, *args) -> float:
-        producer = cast(Producer, args[0])
-        if producer.market is None:
+        if not self.use_imperfect_information:
+            return super().utility(topic, *args)
+        if self.market is None:
             raise ValueError("Producer has no market.")
-        production_rate = cast(float, args[1])
+        production_rate = cast(float, args[0])
+        external_production_rate = cast(float, args[1])
 
-        influencer_reward = 0
-        for influencer in producer.market.influencers:
-            # todo: recalculate influencer following rates
-            influencer_reward += np.exp(-influencer.delay_sensitivity * (1 / influencer.producer_following_rates[producer.index]))
+        prev_topic = self.topic_produced
+        self.topic_produced = topic
         
+        for influencer in self.market.influencers:
+            prev_rate = influencer.get_following_rate_vector()
+            
+            attention_constraint = LinearConstraint(np.ones(self.market.num_agents + 1), lb=0, ub=influencer.attention_bound)
+
+            bounds = []
+            for agent in self.market.agents:
+                if agent == influencer or (not isinstance(agent, Producer) and not isinstance(agent, Influencer)):
+                    bounds.append((0, 0))
+                else:
+                    bounds.append((0, None))
+            bounds.append((influencer.get_following_rate_vector()[-1], influencer.get_following_rate_vector()[-1])) # external should not change when optimizing influencer
+
+            result = minimize(
+                fun=influencer.minimization_utility,
+                x0=influencer.get_following_rate_vector(),
+                args=(production_rate, external_production_rate, OptimizationTargets.INFLUENCER),
+                constraints=attention_constraint,
+                bounds=bounds
+            )
+
+            if not result.success:
+                raise RuntimeError("Optimization failed", result)
+            
+        influencer_reward = 0
+        for influencer in self.market.influencers:
+
+            influencer_reward += np.exp(-influencer.delay_sensitivity * (1 / influencer.following_rates[self.index]))
+
+            influencer.set_following_rate_vector(prev_rate)
+        
+        self.topic_produced = prev_topic
         return influencer_reward
