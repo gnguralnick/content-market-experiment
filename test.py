ae2a1fb4e959196bec2da2b5f76d573922e44521
Gabe Guralnick <gnguralnick@gmail.com>
2023-10-23 16:18:50 -0400

    update test script for imperfect information

--- test.py
+++ test.py
@@ -2,24 +2,23 @@ from agents import *
 from content_market import ContentMarket
 import numpy as np
 
-def test(topics: np.ndarray, varied_param: str, num_producers: int | list[int], num_influencers: int | list[int], num_consumers: int | list[int],
-         producer_topic_func: Callable[[np.ndarray], float] | list[Callable[[np.ndarray], float]], consumer_topic_func: Callable[[np.ndarray], float] | list[Callable[[np.ndarray], float]],
+def test(topics: np.ndarray, varied_param: str, num_agents: int | list[int], num_influencers: int | list[int],
+         topic_interest_func: Callable[[np.ndarray], float] | list[Callable[[np.ndarray], float]],
          consumer_attention_bound: float | list[float], consumer_external_interest_prob: float | list[float], consumer_delay_sensitivity: float | list[float],
          influencer_attention_bound: float | list[float], influencer_delay_sensitivity: float | list[float],
          init_following_rates_method: str = 'random', init_interest_method: str = 'random',
          production_rate: float | list[float] = 1, external_production_rate: float | list[float] = 1,
-         producer_type=Producer, consumer_type=Consumer, influencer_type=Influencer):
+         use_imperfect_information: bool = False):
     """
     Test the market with the given parameters.
     A test will be run for every combination of parameters.
     """
-    # use varied_param to determine which parameter to vary
 
     # create a list of all parameters
-    param_names = ['num_producers', 'num_influencers', 'num_consumers', 'producer_topic_func', 'consumer_topic_func',
+    param_names = ['num_agents', 'num_influencers', 'topic_interest_func',
                    'consumer_attention_bound', 'consumer_external_interest_prob', 'consumer_delay_sensitivity',
                    'influencer_attention_bound', 'influencer_delay_sensitivity', 'production_rate', 'external_production_rate']
-    params = [num_producers, num_influencers, num_consumers, producer_topic_func, consumer_topic_func,
+    params = [num_agents, num_influencers, topic_interest_func,
                 consumer_attention_bound, consumer_external_interest_prob, consumer_delay_sensitivity,
                 influencer_attention_bound, influencer_delay_sensitivity, production_rate, external_production_rate]
     
@@ -49,19 +48,20 @@ def test(topics: np.ndarray, varied_param: str, num_producers: int | list[int], 
 
     # run the test for each combination of parameters
     for combo in param_combinations:
-        [producers, influencers, consumers, prod_topic_func, cons_topic_func,
+        [agents, influencers, topic_interest_func,
          cons_att_bound, cons_ext_prob, cons_delay_sens, influencer_att_bound, influencer_delay_sens,
          prod_rate, ext_prod_rate] = combo
         # create the market
         market = ContentMarket(topics)
         markets.append(market)
-        # create the agents
-        for i in range(producers):
-            market.add_agent(producer_type(prod_topic_func))
+        AgentType = ConsumerProducer
+        if use_imperfect_information:
+            AgentType = ImperfectConsumerProducer
+        for i in range(agents):
+            market.add_agent(AgentType(topic_interest_func, cons_att_bound, cons_ext_prob, cons_delay_sens, init_following_rates_method))
         for i in range(influencers):
-            market.add_agent(influencer_type(influencer_att_bound, influencer_delay_sens, init_following_rates_method))
-        for i in range(consumers):
-            market.add_agent(consumer_type(cons_topic_func, cons_att_bound, cons_ext_prob, cons_delay_sens, init_following_rates_method))
+            market.add_agent(Influencer(influencer_att_bound, influencer_delay_sens, init_following_rates_method))
+        
         # initialize the market
         market.finalize(method=init_interest_method)
         # run the market
